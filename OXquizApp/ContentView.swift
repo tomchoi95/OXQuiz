16f4d0310e23786d00309837875bd3b8bc22c9ae
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-10 13:25:23 +0900

    Initial Commit

---
+++ OXquizApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OXquizApp
+//
+//  Created by 최범수 on 2025-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OXquizApp/OXquizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXquizAppApp.swift
+//  OXquizApp
+//
+//  Created by 최범수 on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct OXquizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
